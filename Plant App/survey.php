9d9b0b10e91651d36400fb3c09459d3271873f1c
Alan Gonzalez <aggonzalez9@cps.edu>
2018-05-03 13:11:35 +0000

    master

--- Plant App/survey.php
+++ Plant App/survey.php
@@ -1,12 +1,34 @@
-<!DOCTYPE html>
+
 <html>
 <body>
 
-<h1>My first PHP page</h1>
+<?php 
+$temp = $_POST["temp"];
+$light = $_POST["light"];
+$bloom = $_POST["bloom"];
+$zone = $_POST["zone"];
+$type = $_POST["type"];
+?>
 
-<?php
-echo "Hello World!";
-?> 
+the temp you chose is: 
+<?php echo $temp; 
+?>
+<br>
 
+the light you chose is: 
+<?php echo $light
+?>
+<br>
+the bloom you chose is: 
+<?php echo $bloom; 
+?>
+<br>
+the zone you chose is: 
+<?php echo $zone; 
+?>
+<br>
+the zone you chose is: 
+<?php echo $type; 
+?>
 </body>
 </html>
